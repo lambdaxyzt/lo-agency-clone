6b9f2092c5cd384fb85b3debd8de7ff3c08d5f0b
Lambdaxyzt <lambdaxyzt@gmail.com>
2021-02-19 01:35:36 +0330

    adding meet aliens section

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
+window.onload = () => {
+  onResize();
+}
+
+
 let cursor = document.querySelector('.cursor');
 document.addEventListener('mousemove',animateProp);
 function animateProp(e) {
     anime({
       targets: cursor,
-      left: e.clientX-15,
-      top: e.clientY-15,
-      duration:50,
+      left: e.pageX-15,
+      top: e.pageY-15,
+      duration:0,
       easing:'linear'
     });
   }
+
+
+
+
+window.addEventListener('resize',onResize);
+let listProfile = document.querySelectorAll('.meet-alien .profiles  .profile-picture');
+let counter1 , sum1 , width1 ;
+
+function onResize() {
+
+  counter1 = 0;
+  for (element of listProfile) {
+    width1 = element.getBoundingClientRect().width; 
+    element.style.height = `${width1}px`;
+    sum1 =  (50 * Math.floor(counter1%3)); 
+    element.style.transform=`translateY(${sum1}px)`
+    counter1++;
+  }
+}
+
+
+
+
+// function onResize() {
+//   for (element of listProfile) {
+//     width1 = element.style.height; 
+//     width1 = `${element.getBoundingClientRect().width}px`;
+//     sum1 =  10 * counter1 + ( width1 * counter1%3 )  
+//     element.transform=`translate(${sum1}px)`
+//   }
+// }
