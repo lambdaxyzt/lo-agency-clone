d45c3328ef88f49a4bf1591c3301d3cbcc13847e
Lambdaxyzt <lambdaxyzt@gmail.com>
2021-03-05 01:02:54 +0330

    change server js file for syupporting css and js

--- server.js
+++ server.js
@@ -4,17 +4,45 @@ const fs = require('fs');
 let port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('./index.html', {
-        encoding: 'utf8'
-    }, (err, data) => {
-        if (!!err)
-            console.log(err.message);
-        else {
-            res.write(data)
-            res.end()
-        }
-    })
+    if(req.url == "./index.html" || req.url == "/" ) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.readFile('./index.html', {
+            encoding: 'utf8'
+        }, (err, data) => {
+            if (!!err)
+                console.log(err.message);
+            else {
+                res.write(data)
+                res.end()
+            }
+        })
+    }
+    if(req.url == "./asset/css/style.css") {
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        fs.readFile(req.url, {
+            encoding: 'utf8'
+        }, (err, data) => {
+            if (!!err)
+                console.log(err.message);
+            else {
+                res.write(data)
+                res.end()
+            }
+        })
+    }
+    if(req.url == "./asset/js/app.js") {
+        res.writeHead(200, {'Content-Type': 'text/javascript'});
+        fs.readFile(req.url, {
+            encoding: 'utf8'
+        }, (err, data) => {
+            if (!!err)
+                console.log(err.message);
+            else {
+                res.write(data)
+                res.end()
+            }
+        })
+    }
 });
 
 server.listen(port, () => {
