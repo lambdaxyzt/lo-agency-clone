279985c4eee3030f9ba440dcfcb9a2ad664dd6aa
Lambdaxyzt <lambdaxyzt@gmail.com>
2021-03-05 00:56:53 +0330

    fix link in  NodeJS Server File

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let port = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('./Views/index.html', {
+    fs.readFile('./index.html', {
         encoding: 'utf8'
     }, (err, data) => {
         if (!!err)
